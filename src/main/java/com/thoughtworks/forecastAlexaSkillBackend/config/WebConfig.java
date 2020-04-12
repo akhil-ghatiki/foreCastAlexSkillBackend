9844d5375c9170f5830ea6bce8ff627e66d66a87
Swathi Varkala <swathiv@Swathis-MacBook-Pro.local>
2020-04-12 16:12:41 +0530

    Swathi | 1 | Make Alexa Skill ID configurable

--- src/main/java/com/thoughtworks/forecastAlexaSkillBackend/config/WebConfig.java
+++ src/main/java/com/thoughtworks/forecastAlexaSkillBackend/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.thoughtworks.forecastAlexaSkillBackend.config;
 import com.amazon.ask.servlet.ServletConstants;
 import com.thoughtworks.forecastAlexaSkillBackend.servlet.AlexaServlet;
 import javax.servlet.http.HttpServlet;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfig {
 
+  @Autowired
+  AppConfig appConfig;
   @Bean
   public ServletRegistrationBean<HttpServlet> alexaServlet() {
     loadProperties();
     ServletRegistrationBean<HttpServlet> servletServletRegistrationBean = new ServletRegistrationBean<>();
-    servletServletRegistrationBean.setServlet(new AlexaServlet());
+    servletServletRegistrationBean.setServlet(new AlexaServlet(appConfig));
     servletServletRegistrationBean.addUrlMappings("/alexa/*");
     servletServletRegistrationBean.setLoadOnStartup(1);
     return servletServletRegistrationBean;
