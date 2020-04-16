4cd90f9a3601b3b1db203d1e7ce2b9877a2b45c1
Swathi Varkala <swathivarkala@gmail.com>
2020-04-17 00:34:32 +0530

    Swathi | REFACTOR, Add Dialogue for OrderIntent

--- src/main/java/com/thoughtworks/forecastAlexaSkillBackend/defaultIntentHandlers/NoIntentHandler.java
+++ src/main/java/com/thoughtworks/forecastAlexaSkillBackend/defaultIntentHandlers/NoIntentHandler.java
@@ -1,8 +1,9 @@
-package com.thoughtworks.forecastAlexaSkillBackend.customIntenthandlers;
+package com.thoughtworks.forecastAlexaSkillBackend.defaultIntentHandlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
+import com.thoughtworks.forecastAlexaSkillBackend.customIntenthandlers.GroceryItemIntentHandler;
 import com.thoughtworks.forecastAlexaSkillBackend.domain.Step;
 
 import java.util.Map;
@@ -24,12 +25,16 @@ public class NoIntentHandler implements RequestHandler {
             return handleGroceryItem(handlerInput);
         }
 
-        if(sessionAttributes.get(Step.CURRENT_STEP) !=null && sessionAttributes.get(Step.CURRENT_STEP).equals(Step.CONFIRM_ORDER)){
+        if(sessionAttributes.get(Step.CURRENT_STEP) !=null && sessionAttributes.get(Step.CURRENT_STEP).equals(Step.CONFIRM_CHECKOUT)){
             speechText = speechText.concat(" Clearing the Cart");
             //TODO Clear Add to Cart List
         }
-        speechText = speechText.concat(", Have a good day");
-       return handlerInput.getResponseBuilder()
+
+        //TODO if items in cart with out checkout: ask for checkout
+
+        else speechText = speechText.concat(" Have a good day");
+
+        return handlerInput.getResponseBuilder()
                .withSpeech(speechText)
                .withSimpleCard("Okay", "Have a good day")
                .build();
---
+++ src/main/java/com/thoughtworks/forecastAlexaSkillBackend/domain/Slots.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.forecastAlexaSkillBackend.domain;
+
+public class Slots {
+    public static String ORDER_NAME_SLOT = "orderNameSlot";
+    public static String ORDER_QUANTITY_SLOT = "orderQuantitySlot";
+    public static String CONFIRM_CHECKOUT_SLOT = "confirmCheckoutSlot";
+}
